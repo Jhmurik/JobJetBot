5220c13b0c905804ff9838bb81af9a486607c81d
Jhmurik <rokosovski_dvor@tut.by>
2025-07-12 14:51:29 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS drivers (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    experience TEXT,
+    license_type TEXT,
+    languages TEXT[],
+    city TEXT,
+    ready_to_work BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

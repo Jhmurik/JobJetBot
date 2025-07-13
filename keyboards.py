0841c588d95432e676df80330960b5588274fbde
Jhmurik <rokosovski_dvor@tut.by>
2025-07-13 20:15:59 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,15 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
-main_menu.add(
-    KeyboardButton("🚛 Найти работу"),
-    KeyboardButton("🏢 Я — компания")
-)
-main_menu.add(
-    KeyboardButton("💼 Профиль"),
-    KeyboardButton("⭐ Подписка Premium")
-)
-main_menu.add(
-    KeyboardButton("🔗 Полезные сервисы"),
-    KeyboardButton("❓ Помощь")
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="/driver")],
+        [KeyboardButton(text="/company")]
+    ],
+    resize_keyboard=True
 )

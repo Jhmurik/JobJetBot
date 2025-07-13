efa2870a884eb3795dfd6a40e1175c6437e8167f
Jhmurik <rokosovski_dvor@tut.by>
2025-07-13 23:13:43 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,20 @@
 import pathlib
+import asyncpg
 
+# Функция: создать таблицы из schema.sql
 async def create_tables(pool):
     schema_path = pathlib.Path("schema.sql")
+    schema_sql = schema_path.read_text(encoding='utf-8')
     async with pool.acquire() as conn:
-        with schema_path.open("r") as f:
-            await conn.execute(f.read())
+        await conn.execute(schema_sql)
+
+# Функция: подключиться к базе (и создать таблицы)
+async def connect_to_db():
+    from os import getenv
+    db_url = getenv("DATABASE_URL")
+    if not db_url:
+        raise ValueError("❌ DATABASE_URL не найден в переменных окружения")
+
+    pool = await asyncpg.create_pool(dsn=db_url)
+    await create_tables(pool)
+    return pool

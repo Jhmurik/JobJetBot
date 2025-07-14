13242d9c16178fcff64996f5f46af2361ca24b41
Jhmurik <rokosovski_dvor@tut.by>
2025-07-14 09:03:32 +0200

    Update stats.py

--- utils/stats.py
+++ utils/stats.py
@@ -4,3 +4,8 @@ async def count_drivers(pool):
     async with pool.acquire() as conn:
         result = await conn.fetchval("SELECT COUNT(*) FROM drivers")
         return result or 0
+
+async def count_companies(pool):
+    async with pool.acquire() as conn:
+        result = await conn.fetchval("SELECT COUNT(*) FROM companies")
+        return result or 0

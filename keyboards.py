f9cda3f5b6e50ad913095be9ad47b431ed7c3104
Jhmurik <rokosovski_dvor@tut.by>
2025-07-16 17:35:40 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,30 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-main_menu = ReplyKeyboardMarkup(
+# Языки
+translations = {
+    "ru": "🇷🇺 Русский",
+    "en": "🇬🇧 English",
+    "uz": "🇺🇿 Oʻzbek",
+    "uk": "🇺🇦 Українська",
+    "hi": "🇮🇳 हिन्दी",
+    "pl": "🇵🇱 Polski"
+}
+
+language_keyboard = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text=lang)] for lang in translations.values()],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+# Главное меню
+main_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="/driver")],
-        [KeyboardButton(text="/company")]
+        [KeyboardButton(text="📝 Заполнить анкету")],
+        [KeyboardButton(text="📦 Для компаний")],
+        [KeyboardButton(text="🌐 Сменить язык")],
+        [KeyboardButton(text="📊 Статистика")],
+        [KeyboardButton(text="🚫 Выключить анкету")],  # 👈 Новая кнопка
+        [KeyboardButton(text="✅ Включить анкету (платно)")]  # 👈 Новая кнопка
     ],
     resize_keyboard=True
 )

81c077ca8bfd7a4a412d6602d17b83ef698dbab8
Jhmurik <rokosovski_dvor@tut.by>
2025-07-19 14:23:53 +0200

    Update manager_state.py

--- states/manager_state.py
+++ states/manager_state.py
@@ -1,13 +1,11 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class ManagerForm(StatesGroup):
-    join_code = State()
     full_name = State()
     position = State()
     phone = State()
     email = State()
     company_name = State()
-    company_description = State()
     company_country = State()
     company_city = State()
     regions = State()

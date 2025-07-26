d4ba22ce654a76a88a1f6e7a5ed8f9cd799774b8
Jhmurik <rokosovski_dvor@tut.by>
2025-07-26 13:18:16 +0200

    Update save_payment.py

--- db/save_payment.py
+++ db/save_payment.py
@@ -1,17 +1,24 @@
 from asyncpg import Pool
+from datetime import datetime
 
 # 💾 Сохранение информации о платеже
 async def save_payment(pool: Pool, payment: dict):
     async with pool.acquire() as conn:
         await conn.execute("""
-            INSERT INTO payments (user_id, role, amount, currency, payment_method, payment_type, description)
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-        """, 
-        payment["user_id"],
-        payment["role"],
-        payment["amount"],
-        payment["currency"],
-        payment["payment_method"],
-        payment["payment_type"],
-        payment["description"]
+            INSERT INTO payments (
+                user_id, role, amount, currency,
+                payment_method, payment_type, description, created_at
+            ) VALUES (
+                $1, $2, $3, $4,
+                $5, $6, $7, $8
+            )
+        """,
+        payment.get("user_id"),
+        payment.get("role"),
+        payment.get("amount"),
+        payment.get("currency", "USDT"),
+        payment.get("payment_method", "cryptomus"),
+        payment.get("payment_type", "premium"),
+        payment.get("description", ""),
+        datetime.utcnow()
                           )

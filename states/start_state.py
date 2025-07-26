396d2636ef94a365194fb2334cc07325f4063b22
Jhmurik <rokosovski_dvor@tut.by>
2025-07-26 17:25:47 +0200

    Update start_state.py

--- states/start_state.py
+++ states/start_state.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class StartState(StatesGroup):
-    language = State()       # 🌐 Выбор языка
-    role = State()           # 👤 Выбор роли (driver | company | manager)
-    regions = State()        # 🌍 Выбор регионов работы
+    language = State()
+    role = State()
+    regions = State()
